7b2bcddef854070e32582a399b4fa9873cc7c608
Loknath Sai Gurijala <loknath.gurijala@gmail.com>
2026-05-03 23:10:54 +0530

    Initial Deployment

--- app.py
+++ app.py
@@ -223,3 +223,7 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}),200
